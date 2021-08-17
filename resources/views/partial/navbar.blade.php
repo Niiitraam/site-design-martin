011c1bae97ade1e50d4614c53a3cd5f78157838c
Martin <martinmdvld@gmail.com>
2021-08-17 13:58:12 +0200

    navbar

--- resources/views/partial/navbar.blade.php
+++ resources/views/partial/navbar.blade.php
@@ -1,10 +1,28 @@
 <div class="taille-nav">
     <ul class="position-ul">
-        <li><a href="#section1" class="lien-navbar">Home</a></li>
-        <li><a href="#section2" class="lien-navbar">A Propos</a></li>
-        <li><a href="#section3" class="lien-navbar">Parcours</a></li>
-        <li><a href="#section4" class="lien-navbar">Services</a></li>
-        <li><a href="" class="lien-navbar">Portfolio</a></li>
-        <li><a href="" class="lien-navbar">Contact</a></li>
+        <div class="div-margin">
+          <li class="span-container s1"><a href="#section1" class="lien-navbar">Home</a></li>
+          <li class="span-container s2"><a href="#section1" class="lien-navbar">Home</a></li>  
+        </div>
+        <div class="div-margin">
+            <li class="span-container s1"><a href="#section2" class="lien-navbar">A Propos</a></li>
+            <li class="span-container s2"><a href="#section2" class="lien-navbar">A Propos</a></li>
+        </div>
+        <div class="div-margin">
+            <li class="span-container s1"><a href="#section3" class="lien-navbar">Parcours</a></li>
+            <li class="span-container s2"><a href="#section3" class="lien-navbar">Parcours</a></li>
+        </div>
+        <div class="div-margin">
+            <li class="span-container s1"><a href="#section4" class="lien-navbar">Services</a></li>
+            <li class="span-container s2"><a href="#section4" class="lien-navbar">Services</a></li>
+        </div>
+        <div class="div-margin">
+            <li class="span-container s1"><a href="" class="lien-navbar">Portfolio</a></li>
+            <li class="span-container s2"><a href="" class="lien-navbar">Portfolio</a></li>
+        </div>
+        <div class="div-margin">
+            <li class="span-container s1"><a href="" class="lien-navbar">Contact</a></li>
+            <li class="span-container s2"><a href="" class="lien-navbar">Contact</a></li>
+        </div>
     </ul>
 </div>
